cd3bbcadd1f346f73024172fb2150e5e7c1b4f15
anishkarn33 <anish.karna133@gmail.com>
2025-05-31 23:05:37 +0545

    feat: add TaskCompletionLog and ProductivityMetrics to module exports

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,4 @@
 from .user import User
 from .task import Task, TaskStatus, TaskPriority
 
-__all__ = ["User", "Task", "TaskStatus", "TaskPriority"]
+__all__ = ["User", "Task", "TaskStatus", "TaskPriority", "TaskCompletionLog", "ProductivityMetrics"]
